ada2fc07cf48a394d2c5c051aecc2673a5af4277
kangfuad <lfuad1223@gmail.com>
2022-07-09 12:18:11 +0700

    fuad

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,6 +54,6 @@ class User extends Authenticatable
 
     public function count_warehouse()
     {
-        return $this->hasMany(Mpe_sob::class, 'id_pic_wharehouse', 'id')->where('is_active', '=', 1);
+        return $this->hasMany(Mpe_rpu_sob::class, 'id_pic_wharehouse', 'id')->where('is_active', '=', 1);
     }
 }
